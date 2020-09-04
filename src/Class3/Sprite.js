41e6d6ffd89a8c6e551540bad1824bf7951fc513
unknown <thanhluan9796@gmail.com>
2020-09-04 18:09:25 +0700

    update

--- src/Class3/Sprite.js
+++
@@ -1,8 +0,0 @@
-//Class Sprite
-export default class Sprite extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, texture, frame) {
-        super(scene, x, y, texture, frame);
-        scene.add.existing(this);
-        this.setInteractive();
-    }
-}
